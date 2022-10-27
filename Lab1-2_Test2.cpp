63814f9452e97d3fc0cbfc1391ab614f2c4967cb
lleixng <116694838+lleixng@users.noreply.github.com>
2022-10-27 15:09:47 +0800

    测试文件2

---
+++ Lab1-2_Test2.cpp
@@ -0,0 +1,52 @@
+#include <stdioh>  
+int main() {
+	int i = 1;
+	double j = 0;
+	long f;
+	switch (i) {
+	case 0:
+		break;
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	default:
+		break;
+	}
+	switch (i) {
+	case 0:
+		break;
+	case 1:
+		break;
+	case 2:
+		break;
+	default:
+		break;
+	}
+	if (i < 0) {
+		if (i < -1) {
+		} else {
+		}
+	} else if (i > 0) {
+		if (i > 2) {
+		} else if (i == 2) {
+		} else if (i > 1) {
+		} else if (i > 2020) {
+		} else {
+		}
+	} else {
+	}
+	if(){
+		
+	}else if(){
+		
+	}else{
+}
+	if (j != 0) {
+	} else {
+	}
+	if(){}
+	return 0;
+}
